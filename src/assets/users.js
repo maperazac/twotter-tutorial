9eb09779e1ce2146cb5ed65a4317f6b69a616abd
maperazac <75704966+maperazac@users.noreply.github.com>
2021-01-23 16:14:52 -0700

    initialize project

---
+++ src/assets/users.js
@@ -0,0 +1,47 @@
+export const users = [
+    {
+        "id": 1,
+        "username": "_MitchellRomney",
+        "firstName": "Mitchell",
+        "lastName": "Romney",
+        "email": "mitchellromney@theearthissquare.com",
+        "isAdmin": true,
+        "twoots": [
+            {
+                "id": 1,
+                "content": "Twooter is Amazing!"
+            },
+            {
+                "id": 2,
+                "content": "Don't forget to subscribe to The Earth is Square!"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "username": "JColeNC",
+        "firstName": "J.",
+        "lastName": "Cole",
+        "email": null,
+        "isAdmin": false,
+        "twoots": []
+    },
+    {
+        "id": 3,
+        "username": "kurtisconner",
+        "firstName": "Kurtis",
+        "lastName": "Conner",
+        "email": null,
+        "isAdmin": false,
+        "twoots": []
+    },
+    {
+        "id": 4,
+        "username": "boburnham",
+        "firstName": "Bo",
+        "lastName": "Burnham",
+        "email": "null",
+        "isAdmin": false,
+        "twoots": []
+    }
+];
